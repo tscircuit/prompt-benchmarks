38bba83eface166a2bdfc28a37a028cb1d7ec3be
seveibar <seveibar@gmail.com>
2024-10-06 10:45:06 -0700

    Add threshold test script for verifying correctness threshold

---
+++ scripts/threshold-test.ts
@@ -0,0 +1,7 @@
+// This script runs "bun test ..." against all the files
+// in the specified directory and it's subdirectories the
+// designated number of times and expects the correctness
+// threshold to be met
+
+// example usage:
+// bun run scripts/threshold-test.ts tests/smoke --times 10 --threshold 0.9
