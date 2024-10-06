9e95d8cad9fdef94e44411b90b026139d32445ef
seveibar <seveibar@gmail.com>
2024-10-06 14:37:00 -0700

    get imports from code function

--- lib/code-runner/get-imports-from-code.ts
+++ lib/code-runner/get-imports-from-code.ts
@@ -1,10 +1,13 @@
 export const getImportsFromCode = (code: string): string[] => {
-  const importRegex = /import\s+["'](.*?)["']/g
-  const imports = []
-  let match: any
+  const importRegex =
+    /import\s+(?:(?:(?:\*\s+as\s+\w+)|(?:[\w\s,{}]+))\s+from\s+)?['"](.+?)['"]/g
+  const imports: string[] = []
+  let match: RegExpExecArray | null
+
   // biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
-  while ((match = importRegex.exec(code))) {
+  while ((match = importRegex.exec(code)) !== null) {
     imports.push(match[1])
   }
+
   return imports
 }
---
+++ tests/lib/get-imports-from-code.test.ts
@@ -0,0 +1,57 @@
+import { expect, test } from "bun:test"
+import { getImportsFromCode } from "../../lib/code-runner/get-imports-from-code"
+
+test("getImportsFromCode with various import styles", () => {
+  const testCode = `
+    import React from 'react'
+    import { useState, useEffect } from 'react'
+    import axios from 'axios'
+    import * as utils from './utils'
+    import type { User } from './types'
+    import './styles.css'
+    
+    const Component = () => {
+      // Component code here
+    }
+  `
+
+  const imports = getImportsFromCode(testCode)
+
+  expect(imports).toEqual(['react', 'react', 'axios', './utils', './types', './styles.css'])
+})
+
+test("getImportsFromCode with no imports", () => {
+  const testCode = `
+    const x = 5
+    function test() {
+      return x * 2
+    }
+  `
+
+  const imports = getImportsFromCode(testCode)
+
+  expect(imports).toEqual([])
+})
+
+test("getImportsFromCode with mixed quotes", () => {
+  const testCode = `
+    import React from "react"
+    import { useState } from 'react'
+    import axios from "axios"
+  `
+
+  const imports = getImportsFromCode(testCode)
+
+  expect(imports).toEqual(['react', 'react', 'axios'])
+})
+
+test("getImportsFromCode with side-effect imports", () => {
+  const testCode = `
+    import 'dotenv/config'
+    import './polyfills'
+  `
+
+  const imports = getImportsFromCode(testCode)
+
+  expect(imports).toEqual(['dotenv/config', './polyfills'])
+})
