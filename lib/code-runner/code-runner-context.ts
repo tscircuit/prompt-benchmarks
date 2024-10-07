88269a34f3225c20a32c1852bef76ab745a708f4
seveibar <seveibar@gmail.com>
2024-10-06 17:21:18 -0700

    define CodeRunnerContext

---
+++ lib/code-runner/code-runner-context.ts
@@ -0,0 +1,26 @@
+export type Model = "claude-3-5-sonnet-20240620" | "claude-3-haiku-20240307"
+export type OutputType = "board" | "footprint" | "package" | "model"
+
+export interface PromptContext {
+  /**
+   * Imports available to the AI
+   */
+  availableImports?: Record<string, string>
+
+  model: Model
+
+  outputType: OutputType
+}
+
+export interface CodeRunnerContext {
+  /**
+   * Imports that are accessible when running the code with their imported
+   * values.
+   */
+  preSuppliedImports?: Record<string, any>
+
+  /**
+   * The type of output to generate.
+   */
+  outputType: OutputType
+}
