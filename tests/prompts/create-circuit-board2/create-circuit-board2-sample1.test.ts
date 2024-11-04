50dd4c09103c8c092147268d2cb9b11b3b791b8a
seveibar <seveibar@gmail.com>
2024-11-03 17:29:33 -0800

    prompt2 with virtual filesystem and tscircuit snippets integration

---
+++ tests/prompts/create-circuit-board2/create-circuit-board2-sample1.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "bun:test"
+import { runInitialPrompt } from "lib/code-runner/run-prompt"
+import { createCircuitBoard1Template } from "prompt-templates/create-circuit-board1"
+import { boardSample1 } from "tests/board-samples/sample1"
+
+test("create-circuit-board1-prompt1", async () => {
+  const systemPrompt = createCircuitBoard1Template({ currentCode: "" })
+
+  const { success, circuit } = await runInitialPrompt(
+    { systemPrompt, userPrompt: boardSample1 },
+    {
+      model: "claude-3-haiku-20240307",
+      outputType: "board",
+    },
+  )
+
+  expect(success).toBe(true)
+
+  const led = circuit?.selectOne("led")
+
+  expect(led).toBeDefined()
+})
