14128e1521cc777977787351921dd3878d6ca1d8
seveibar <seveibar@gmail.com>
2024-11-07 14:56:48 -0800

    module example

--- tests/prompts/create-circuit-module1/create-circuit-module1-sample1.test.ts
+++ tests/prompts/create-circuit-module1/create-circuit-module1-sample1.test.ts
@@ -3,11 +3,11 @@ import { runInitialPrompt } from "lib/code-runner/run-prompt"
 import { createCircuitBoard1Template } from "prompt-templates/create-circuit-board1"
 import { moduleSample1 } from "tests/module-samples/sample1-na555"
 
-test("create-circuit-board1-prompt1", async () => {
+test("create-circuit-module1-prompt1", async () => {
   const systemPrompt = createCircuitBoard1Template({ currentCode: "" })
 
   const { success, circuit } = await runInitialPrompt(
-    { systemPrompt, userPrompt: boardSample1 },
+    { systemPrompt, userPrompt: moduleSample1 },
     {
       model: "claude-3-haiku-20240307",
       outputType: "board",
